d2ca4b3561378b45362c7728c979c20dc52eb48a
bonitachen987 <bonitachen1@outlook.com>
2020-04-20 19:33:43 +1000

    bonita commit

---
+++ app/src/main/java/com/example/groupassignment/backProgress.java
@@ -0,0 +1,46 @@
+package com.example.groupassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class backProgress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_back_progress);
+
+
+        TextView yourMark = findViewById(R.id.yourMarkBack);
+        TextView highestMark = findViewById(R.id.highestMarkBack);
+
+        TextView attempt = findViewById(R.id.attemptBack);
+        //TextView attemptValue = findViewById(R.id.attemptValue);
+
+        //get the score from the quiz by Intent
+        Intent intent = getIntent();
+        int marks = intent.getIntExtra("marks", 0);
+        //int count = intent.getIntExtra("count", 0);
+        yourMark.setText("Your Mark: " + marks);
+        //attempt.setText("Attempt: " + count);
+
+
+        //use SharedPreferences to save the best score
+        SharedPreferences mypref = getPreferences(MODE_PRIVATE);
+        int highscore = mypref.getInt("highscore", 0);
+        if (highscore>= marks){
+            highestMark.setText("Highest Mark: " +highscore);
+        }
+        else{
+            highestMark.setText("New Highest Mark: " +marks);
+            SharedPreferences.Editor editor = mypref.edit();
+            editor.putInt("highscore", marks);
+            editor.commit();
+        }
+
+    }
+}
